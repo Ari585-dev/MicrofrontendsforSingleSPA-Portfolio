b22da95bf19c96a6e2da875b78313580d4d0f7ca
Ari585-dev <129306585+Ari585-dev@users.noreply.github.com>
2023-06-23 12:06:35 -0500

    Initial commit

---
+++ mf-presentation/src/main.single-spa.ts
@@ -0,0 +1,33 @@
+
+import { enableProdMode, NgZone } from '@angular/core';
+
+
+import { getSingleSpaExtraProviders, singleSpaAngular } from 'single-spa-angular';
+
+
+import { environment } from './environments/environment';
+import { singleSpaPropsSubject } from './single-spa/single-spa-props';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+const lifecycles = singleSpaAngular({
+  bootstrapFunction: singleSpaProps => {
+    singleSpaPropsSubject.next(singleSpaProps);
+    return bootstrapApplication(AppComponent, {
+      providers: [
+        getSingleSpaExtraProviders()
+      ]
+    })
+   /*  .bootstrapModule(AppModule); */
+  },
+  template: '<app-root />',
+  NgZone,
+});
+
+export const bootstrap = lifecycles.bootstrap;
+export const mount = lifecycles.mount;
+export const unmount = lifecycles.unmount;
