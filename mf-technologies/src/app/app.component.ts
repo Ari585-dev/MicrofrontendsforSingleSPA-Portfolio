774099047651a4a81b106d8dbf6c2efda5d1027b
Ari585-dev <129306585+Ari585-dev@users.noreply.github.com>
2023-07-18 14:57:05 -0500

    presentation and technologies are now responsives

--- mf-technologies/src/app/app.component.ts
+++ mf-technologies/src/app/app.component.ts
@@ -13,11 +13,11 @@ export class AppComponent {
     {id: 1, name: 'Angular', image:'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png'},
     {id: 2, name: 'NodeJS', image:'https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_f0b606abb6d19089febc9faeeba5bc05/nodejs-development-services.png'},
     {id: 3, name: '.NET core', image:'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/.NET_Core_Logo.svg/2048px-.NET_Core_Logo.svg.png'},
-    {id: 4, name: 'Postman', image:'https://img.uxwing.com/wp-content/themes/uxwing/download/brands-social-media/postman-icon.png'},
+    {id: 4, name: 'Postman', image:'https://user-images.githubusercontent.com/7853266/44114706-9c72dd08-9fd1-11e8-8d9d-6d9d651c75ad.png'},
     {id: 5, name: 'SQL', image:'https://www.freeiconspng.com/thumbs/sql-server-icon-png/sql-server-icon-png-29.png'},
     {id: 6, name: 'MongoDB', image:'https://www.pngall.com/wp-content/uploads/13/Mongodb-PNG-Image-HD.png'},
-    {id: 7, name: 'GitHub', image:'https://img.freepik.com/free-icon/github_318-566791.jpg?w=360'},
-    {id: 8, name: 'Java', image:'https://assets.stickpng.com/images/58480979cef1014c0b5e4901.png'}
+    {id: 7, name: 'GitHub', image:'https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-512.png'},
+    {id: 8, name: 'Java', image:'https://icon-library.com/images/java-icon-png/java-icon-png-15.jpg'}
   ];
 
   index=0;
